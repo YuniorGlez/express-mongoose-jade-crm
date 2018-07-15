b57c643f6d2a9280eb05d5e74a884cfc35740d5c
José Luis Santana <yuniorglez@Mac-Yunior.local>
2018-07-15 17:08:17 +0100

    installing pug and first initialization

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+mongoose.connect('mongodb://localhost/db-crm-jade');
 const app = express();
+
+
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/db-crm-jade');
+app.set('views' , './views')
+app.set('view engine' , 'pug')
+
+
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 const customersRouter = require('./api/customers');
-
 app.use('/api/customers', customersRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hola mundo')
-})
 
 app.listen(5000, (err) => {
   if (!err) console.log('Server listening in port 5000');
